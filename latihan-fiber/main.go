624a62aed3bc6f1e817ff80e388fc4ef61c25a06
Rasta Listiadi <rstazz06@gmail.com>
2026-09-06 15:29:20 +0700

    Add Laravel and Fiber projects

---
+++ latihan-fiber/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func main() {
+
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Halo Pemrograman Web II")
+	})
+
+	app.Get("/api/info", func(c fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"aplikasi": "Latihan Fiber",
+			"versi":    "1.0.0",
+			"status":   "berjalan",
+		})
+	})
+
+	app.Get("/api/mahasiswa", func(c fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"nim":            "H1H024053",
+			"nama":           "Rasta Listiadi",
+			"program_studi": "Teknik Komputer",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
---
+++ latihan-laravel/resources/views/namanim.blade.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rasta Listiadi</title>
+</head>
+<body>
+    <div>
+        <h1>Rasta Listiadi</h1>
+        <h2>H1H024053</h2>
+</body>
+</html>
